124999959cc3a7c6e64736aef648918c371cb269
pschuongvtt <huongthukd@gmail.com>
2023-03-19 20:10:34 +0700

    ThuHuong
    Cú pháp Razor trong ASP.NET Core

---
+++ SourceVietNam/SourceVietNam_Version02/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SourceVietNam_Version02.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        //Khai báo 1 Property 
+        public string chuoi { get; set; }
+        public void OnGet()
+        {
+            //Thiết lập thông tin chuỗi 
+            chuoi = "Model - Sử dụng Model trong OnGet()";
+        }
+    }
+}
